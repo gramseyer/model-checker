9e702de3f748678ce965cd5fb62ad1ce765bfe4b
Geoff Ramseyer <geoff.ramseyer@cs.stanford.edu>
2025-06-05 15:26:13 -0700

    big test

--- model_checker/threadpool_test.cc
+++ model_checker/threadpool_test.cc
@@ -8,6 +8,8 @@
 
 namespace model {
 
+// Deliberately keeping test small to test edge cases of
+// not enough work to steal and simple shutdown.
 TEST(ThreadPool, Basic) {
   ThreadPool<int, int> pool(4);
   std::shared_ptr<ExperimentBuilder<int, int>> experiment =
@@ -37,5 +39,69 @@ TEST(ThreadPool, Basic) {
 
   pool.run(experiment);
 }
+
+TEST(ThreadPool, Stealing) {
+  ThreadPool<int, int> pool(4);
+  std::shared_ptr<ExperimentBuilder<int, int>> experiment =
+      std::make_shared<ExperimentBuilder<int, int>>(
+          std::make_tuple(0, 0),
+          [](WorkQueue &work_queue, int &a, int &b) {
+            auto actions = std::make_unique<RunnableActionSet>(work_queue);
+
+            actions->add_action(
+                [](RunnableActionSet &set, int &a, int &b) -> Async {
+                  co_await set.bg();
+                  a += 10;
+                  co_await set.bg();
+                  b += 5;
+                },
+                a, b);
+            
+            actions->add_action(
+                [](RunnableActionSet &set, int &a, int &b) -> Async {
+                  co_await set.bg();
+                  a += 12;
+                  co_await set.bg();
+                  b += 7;
+                },
+                a, b);
+            actions->add_action(
+                [](RunnableActionSet &set, int &a, int &b) -> Async {
+                  co_await set.bg();
+                  a += 8;
+                  co_await set.bg();
+                  b += 3;
+                },
+                a, b);
+            actions->add_action(
+                [](RunnableActionSet &set, int &a, int &b) -> Async {
+                  co_await set.bg();
+                  a += 1;
+                  co_await set.bg();
+                  b += 1;
+                },
+                a, b);
+            actions->add_action(
+                [](RunnableActionSet &set, int &a, int &b) -> Async {
+                  co_await set.bg();
+                  a += 2;
+                  co_await set.bg();
+                  b += 2;
+                },
+                a, b);
+
+            return actions;
+          },
+          [](ActionResult res, int &a, int &b) -> bool {
+            if (res != ActionResult::OK) {
+              return false;
+            }
+
+            return a == 33 && b == 18;
+          });
+
+  pool.run(experiment);
+}
+
 
 } // namespace model
